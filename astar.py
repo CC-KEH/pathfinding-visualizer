7a81b5e1b781b70ca3d12a25b3065844502e551e
CC-KEH <arbashhussain08@gmail.com>
2023-04-15 23:51:37 +0530

    board done now gotta write algos

--- astar.py
+++ astar.py
@@ -1,56 +1,20 @@
-import pygame
-import math
 from queue import PriorityQueue
+from board import *
 
-#Setting up the board
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH,WIDTH))
-pygame.display.set_caption(title="A* Algorithm")
+def heuristic(p1,p2):
+    x1,y1 = p1
+    x2,y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
 
-#Defining colors
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165 ,0)
-GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
-
-
-class Node:
-    def __init__(self,row,col,width,total_rows):
-        self.row = row
-        self.col = col
-        self.x = row*width
-        self.y = col*width
-        self.color = WHITE
-        self.width = width
-        self.neighbors = []
-        self.total_rows = total_rows
-
-    def get_pos(self):
-        return self.row,self.col
-
-    def is_visited(self):
-        #*Visited node is marked colored as RED
-        return self.color == RED
+def algorithm(draw_func,grid,start,end):
+    count = 0
+    open_set = PriorityQueue()
+    #* Inserting F(n) ie FScore,count to keep the check of which node was inserted first,the start node in the open priority queue 
+    open_set.put((0,count,start))
+    came_from = {}
+    #* Assigning G-Score for each node
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
     
-    def is_open(self):
-        return self.color == GREEN
-    
-    def is_obstacle(self):
-        return self.color == BLACK
-    
-    def is_start(self):
-        return self.color == ORANGE
-    
-    def is_end(self):
-        return self.color == PURPLE
-    
-    def reset(self):
-        return self.color == WHITE
-    
-    
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = heuristic(start.get_pos(),end.get_pos())
---
+++ board.py
@@ -0,0 +1,184 @@
+import pygame
+import math
+import astar
+from constants import *
+#Setting up the grid
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH,WIDTH))
+pygame.display.set_caption("A* Path Finding")
+
+#Defining colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+
+
+class Node:
+    def __init__(self,row,col,width,total_rows):
+        self.row = row
+        self.col = col
+        self.x = row*width
+        self.y = col*width
+        self.color = WHITE
+        self.width = width
+        self.neighbors = []
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row,self.col
+
+    def is_visited(self):
+        #*Visited node is marked colored as RED
+        return self.color == RED
+    
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_obstacle(self):
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQUOISE
+    
+    def reset(self):
+        self.color = WHITE
+    
+    def mark_visited(self):
+        self.color = RED
+    
+    def mark_open(self):
+        self.color = GREEN
+    
+    def mark_start(self):
+        self.color = ORANGE
+
+    def mark_obstacle(self):
+        self.color = BLACK
+
+    def mark_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self,win):
+        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.width))
+
+    def update_neighbors(self,grid):
+        self.neighbors = []
+        #* If not an obstacle then add the node to the neighbor list of the current node, Checking in each direction with edge case
+        if self.row < self.total_rows-1 and not grid[self.row+1][self.col].is_obstacle(): #* Down
+            self.neighbors.append(grid[self.row+1][self.col])
+        
+        if self.row > 0 and not grid[self.row-1][self.col].is_obstacle(): #* Up
+            self.neighbors.append(grid[self.row-1][self.col])
+
+        if self.col > 0 and not grid[self.row][self.col-1].is_obstacle(): #* Left
+            self.neighbors.append(grid[self.row][self.col-1])
+        
+        if self.col < self.total_rows-1 and not grid[self.row][self.col+1].is_obstacle(): #* Right
+            self.neighbors.append(grid[self.row][self.col+1])
+
+
+
+    def __lt__(self,other):
+        return False
+    
+def heuristic(p1,p2):
+    x1,y1 = p1
+    x2,y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def make_grid(rows,width):
+    grid = []
+    node_width = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i,j,node_width,rows)
+            grid[i].append(node)
+    
+    return grid
+
+def draw_grid(win,rows,width):
+    node_width = width // rows
+    for i in range(rows):
+        pygame.draw.line(win,GREY,(0,i*node_width),(width,i*node_width))
+        for j in range(rows):
+            pygame.draw.line(win,GREY,(j*node_width,0),(j*node_width,width))
+
+def draw(win,grid,rows,width):
+    win.fill(WHITE)
+    for row in grid:
+        for node in row:
+            node.draw(win)
+    draw_grid(win,rows,width)
+    pygame.display.update()
+
+def get_clicked_pos(pos,rows,width):
+    node_width = width//rows
+    y,x = pos
+    row = y // node_width
+    col = x // node_width
+    return row,col
+
+def main(win,width):
+    ROWS = 50
+    grid = make_grid(ROWS,width)
+    start = None
+    end = None 
+    run = True
+    started = False
+    while run:
+        draw(win,grid,ROWS,width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if started:
+                continue
+            if pygame.mouse.get_pressed()[0]:   #*Left Button
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos,ROWS,width)
+                node = grid[row][col]
+                if not start and node!=end:
+                    start = node
+                    start.mark_start()
+                
+                elif not end and node!=start:
+                    end = node
+                    end.mark_end()
+                
+                elif node!=end and node!=start:
+                    node.mark_obstacle()
+
+            elif pygame.mouse.get_pressed()[2]:  #*Right Button
+                pos = pygame.mouse.get_pos()
+                row,col = get_clicked_pos(pos,ROWS,width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors()
+                    #TODO: Check without using lambda function
+                    astar(lambda:draw(win,grid,ROWS,width),grid,start,end)
+
+    pygame.quit()
+
+main(WIN,WIDTH)
